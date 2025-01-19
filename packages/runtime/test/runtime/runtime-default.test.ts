2f316a52470679c8329b1601d6928acfc713bc1e
leermao <447552708@qq.com>
2025-01-19 15:32:28 +0800

    chore: code review

--- packages/runtime/test/runtime/runtime-default.test.ts
+++ packages/runtime/test/runtime/runtime-default.test.ts
@@ -1,11 +1,11 @@
-import {expect, test} from 'bun:test'
+import { expect, test } from "bun:test";
 
-import {AIGNERuntime} from '../../src'
+import { AIGNERuntime } from "../../src";
 
-test('Runtime.default', async () => {
-  const runtime = new AIGNERuntime()
+test("Runtime.default", async () => {
+  const runtime = new AIGNERuntime();
 
-  expect(runtime.id).toEqual('default-runtime')
-  expect(runtime.state).toEqual({})
-  expect(runtime.name).toBeUndefined()
-})
+  expect(runtime.id).toEqual("default-runtime");
+  expect(runtime.state).toEqual({});
+  expect(runtime.name).toBeUndefined();
+});

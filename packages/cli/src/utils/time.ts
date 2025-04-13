a7d9ec433655d9c3d7b732c93409e9b1ebd716c1
Yechao Li <liyechao@yechao.xyz>
2025-04-14 00:18:22 +0800

    fix: correct parse time duration & add tests

--- packages/cli/src/utils/time.ts
+++ packages/cli/src/utils/time.ts
@@ -1,20 +1,15 @@
 export function parseDuration(duration: number): string {
+  const milliseconds = duration % 1000;
   const seconds = Math.floor(duration / 1000);
   const minutes = Math.floor(seconds / 60);
 
-  if (seconds === 0 && minutes === 0) {
-    const ms = Math.round(duration / 10)
-      .toString()
-      .padStart(2, "0");
-    const s = Number.parseFloat(`0.${ms}`);
-    return `${s}s`;
-  }
+  const ms = Math.round(milliseconds / 10)
+    .toString()
+    .padStart(2, "0");
 
-  let parsedTime = `${seconds % 60}s`;
+  const s = `${Number.parseFloat(`${seconds % 60}.${ms}`)}s`;
 
-  if (minutes > 0) {
-    parsedTime = `${minutes}m${parsedTime}`;
-  }
+  if (minutes === 0) return s;
 
-  return parsedTime;
+  return `${minutes}m${s}`;
 }
---
+++ packages/cli/test/tracer/terminal.test.ts
@@ -0,0 +1,43 @@
+import { expect, spyOn, test } from "bun:test";
+import { TerminalTracer } from "@aigne/cli/tracer/terminal.js";
+import { AIAgent, ExecutionEngine, createMessage } from "@aigne/core";
+
+test("TerminalTracer should work correctly", async () => {
+  const engine = new ExecutionEngine();
+  const context = engine.newContext();
+
+  const testAgent = AIAgent.from({});
+
+  spyOn(testAgent, "process").mockReturnValue(
+    Promise.resolve(createMessage("hello, this is a test response message")),
+  );
+
+  const userAgent = engine.call(testAgent);
+
+  const tracer = new TerminalTracer(context);
+
+  const { result } = await tracer.run(userAgent, createMessage("hello"));
+
+  expect(result).toEqual(
+    expect.objectContaining({
+      $message: "hello, this is a test response message",
+    }),
+  );
+});
+
+test("TerminalTracer should raise error correctly", async () => {
+  const engine = new ExecutionEngine();
+  const context = engine.newContext();
+
+  const testAgent = AIAgent.from({});
+
+  spyOn(testAgent, "process").mockReturnValue(Promise.reject(new Error("test error")));
+
+  const userAgent = engine.call(testAgent);
+
+  const tracer = new TerminalTracer(context);
+
+  const result = tracer.run(userAgent, createMessage("hello"));
+
+  expect(result).rejects.toThrowError("test error");
+});

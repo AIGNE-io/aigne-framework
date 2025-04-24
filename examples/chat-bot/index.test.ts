406b7aa480c77606141061700778b4b55e430f5b
Jason <kamui.huang.js@gmail.com>
2025-04-24 21:42:37 +0800

    Revert "feat(tests): remove console to test ci"
    This reverts commit 32364e679b3c9840b5c5eb45a08cf5c79aaa80d0.

--- examples/chat-bot/index.test.ts
+++ examples/chat-bot/index.test.ts
@@ -4,7 +4,8 @@ import { runExampleTest } from "@aigne/test-utils/run-example-test.js";
 test(
   "should successfully run the chatbot",
   () => {
-    const { status } = runExampleTest();
+    const { stdout, status } = runExampleTest();
+    console.log(stdout.toString());
     expect(status).toBe(0);
   },
   { timeout: 60000 },

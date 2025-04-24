32364e679b3c9840b5c5eb45a08cf5c79aaa80d0
Jason <kamui.huang.js@gmail.com>
2025-04-24 21:40:46 +0800

    feat(tests): remove console to test ci

--- examples/chat-bot/index.test.ts
+++ examples/chat-bot/index.test.ts
@@ -4,8 +4,7 @@ import { runExampleTest } from "@aigne/test-utils/run-example-test.js";
 test(
   "should successfully run the chatbot",
   () => {
-    const { stdout, status } = runExampleTest();
-    console.log(stdout.toString());
+    const { status } = runExampleTest();
     expect(status).toBe(0);
   },
   { timeout: 60000 },

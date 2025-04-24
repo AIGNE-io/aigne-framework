15246df04ac9ad69fd3f1846d53479e29e387268
Jason <kamui.huang.js@gmail.com>
2025-04-24 22:23:32 +0800

    feat(tests): update test cases to use 'output' instead of 'stdout'

--- examples/workflow-handoff/index.test.ts
+++ examples/workflow-handoff/index.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "bun:test";
 import { runExampleTest } from "@aigne/test-utils/run-example-test.js";
 
 test("should successfully run the workflow-handoff", () => {
-  const { stdout, status } = runExampleTest();
-  console.log(stdout.toString());
+  const { output, status } = runExampleTest();
+  console.log(output.toString());
   expect(status).toBe(0);
 });
--- examples/workflow-reflection/index.test.ts
+++ examples/workflow-reflection/index.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "bun:test";
 import { runExampleTest } from "@aigne/test-utils/run-example-test.js";
 test("should successfully run the workflow-reflection", () => {
-  const { stdout, status } = runExampleTest();
-  console.log(stdout.toString());
+  const { output, status } = runExampleTest();
+  console.log(output.toString());
   expect(status).toBe(0);
 });

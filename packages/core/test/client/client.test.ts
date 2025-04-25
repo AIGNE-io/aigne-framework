819bf9d1dacdcbb615147a3a62489745fc62f92c
Yechao Li <liyechao@yechao.xyz>
2025-04-25 19:19:25 +0800

    test: add hono server test case

--- packages/core/test/client/client.test.ts
+++ packages/core/test/client/client.test.ts
@@ -1,105 +1,183 @@
-import { afterEach, beforeEach, expect, spyOn, test } from "bun:test";
+import { expect, spyOn, test } from "bun:test";
 import assert from "node:assert";
-import type { Server } from "node:http";
-import { AIAgent, type ChatModel, ExecutionEngine, type Message } from "@aigne/core";
+import {
+  AIAgent,
+  type AgentCallOptions,
+  ChatModel,
+  ExecutionEngine,
+  type Message,
+} from "@aigne/core";
 import { AIGNEClient } from "@aigne/core/client/client.js";
 import { OpenAIChatModel } from "@aigne/core/models/openai-chat-model.js";
-import { AIGNEServer } from "@aigne/core/server/server.js";
+import { AIGNEServer } from "@aigne/core/server/server";
 import { arrayToReadableStream, readableStreamToArray } from "@aigne/core/utils/stream-utils";
+import { serve } from "bun";
 import { detect } from "detect-port";
 import express from "express";
+import { Hono } from "hono";
 
-let port: number;
-let model: ChatModel;
-let httpServer: Server;
-let url: string;
+interface Server {
+  url: string;
+  aigne: ExecutionEngine;
+  close: () => unknown;
+}
 
-beforeEach(async () => {
-  port = await detect();
+const servers: { name: string; createServer: () => Promise<Server> }[] = [
+  { name: "express", createServer: createExpressServer },
+  { name: "hono", createServer: createHonoServer },
+];
+const options: AgentCallOptions[] = [{ streaming: true }, { streaming: false }];
 
-  model = new OpenAIChatModel();
+const table = servers.flatMap((server) =>
+  options.map((options) => [options, server.name, server.createServer] as const),
+);
 
-  const chat = AIAgent.from({
-    name: "chat",
-  });
+test.each(table)(
+  "AIGNEClient should return correct process options %p for %s server",
+  async (options, _, createServer) => {
+    const { url, close } = await createServer();
+    try {
+      const client = new AIGNEClient({ url });
+      const response = await client.call("chat", { $message: "hello" }, options);
 
-  const engine = new ExecutionEngine({ model, agents: [chat] });
+      if (options.streaming) {
+        assert(response instanceof ReadableStream);
+        expect(readableStreamToArray(response)).resolves.toMatchSnapshot();
+      } else {
+        expect(response).toMatchSnapshot();
+      }
+    } finally {
+      await close();
+    }
+  },
+);
 
-  const aigneServer = new AIGNEServer(engine);
+test.each(table)(
+  "AIGNEClient should return error with options %p for %s server",
+  async (options, _, createServer) => {
+    const { url, aigne, close } = await createServer();
+
+    try {
+      assert(aigne.model instanceof ChatModel);
+
+      spyOn(aigne.model, "process").mockReturnValueOnce(
+        Promise.resolve(
+          arrayToReadableStream([
+            { delta: { text: { text: "Hello" } } },
+            { delta: { text: { text: ", world" } } },
+            new Error("test llm model error"),
+          ]),
+        ),
+      );
+
+      const client = new AIGNEClient({ url });
+      const response = client.call("chat", { $message: "hello" }, options);
+
+      if (options.streaming) {
+        const stream = await response;
+        assert(stream instanceof ReadableStream);
+        expect(readableStreamToArray(stream, { catchError: true })).resolves.toMatchSnapshot();
+      } else {
+        expect(response).rejects.toThrow("test llm model error");
+      }
+    } finally {
+      await close();
+    }
+  },
+);
+
+test.each(table)(
+  "AIGNEClient should return error 'not found agent' with options %p for %s server",
+  async (options, _, createServer) => {
+    const { url, close } = await createServer();
+
+    try {
+      const client = new AIGNEClient({ url });
+
+      const response = client.call("not-exists-agent", {}, options);
+
+      expect(response).rejects.toThrow("status 404: Agent not-exists-agent not found");
+    } finally {
+      await close();
+    }
+  },
+);
+
+test.each(table)(
+  "AIGNEClient should return error 'invalid request body' with options %p for %s server",
+  async (options, _, createServer) => {
+    const { url, close } = await createServer();
+
+    try {
+      const client = new AIGNEClient({ url });
+
+      const response = client.call("chat", [] as unknown as Message, options);
+
+      expect(response).rejects.toThrow(
+        "status 400: Call agent chat check arguments error: input: Expected object, received array",
+      );
+    } finally {
+      await close();
+    }
+  },
+);
+
+async function createExpressServer() {
+  const port = await detect();
+  const url = `http://localhost:${port}/aigne/call`;
 
   const server = express();
 
   server.use(express.json());
 
+  const aigne = await createAIGNE();
+  const aigneServer = new AIGNEServer(aigne);
+
   server.post("/aigne/call", async (req, res) => {
     await aigneServer.call(req.body, res);
   });
 
-  httpServer = server.listen(port);
+  const httpServer = server.listen(port);
 
-  url = `http://localhost:${port}/aigne/call`;
-});
+  return {
+    url,
+    aigne,
+    close: () => {
+      httpServer.closeAllConnections();
+      httpServer.close();
+    },
+  };
+}
 
-afterEach(async () => {
-  httpServer.closeAllConnections();
-  httpServer.close();
-});
+async function createHonoServer() {
+  const port = await detect();
+  const url = `http://localhost:${port}/aigne/call`;
 
-test.each([{ streaming: false }, { streaming: true }])(
-  "AIGNEClient should return correct response for %p",
-  async (options) => {
-    const client = new AIGNEClient({ url });
-    const response = await client.call("chat", { $message: "hello" }, options);
+  const honoApp = new Hono();
 
-    if (options.streaming) {
-      assert(response instanceof ReadableStream);
-      expect(readableStreamToArray(response)).resolves.toMatchSnapshot();
-    } else {
-      expect(response).toMatchSnapshot();
-    }
-  },
-);
+  const aigne = await createAIGNE();
+  const aigneServer = new AIGNEServer(aigne);
 
-test.each([{ streaming: false }, { streaming: true }])(
-  "AIGNEClient should return error response for %p",
-  async (options) => {
-    spyOn(model, "process").mockReturnValueOnce(
-      Promise.resolve(
-        arrayToReadableStream([
-          { delta: { text: { text: "Hello" } } },
-          { delta: { text: { text: ", world" } } },
-          new Error("test error"),
-        ]),
-      ),
-    );
+  honoApp.post("/aigne/call", async (c) => {
+    const body = await c.req.json();
+    return aigneServer.call(body);
+  });
 
-    const client = new AIGNEClient({ url });
-    const response = client.call("chat", { $message: "hello" }, options);
+  const server = serve({ port, fetch: honoApp.fetch });
 
-    if (options.streaming) {
-      const stream = await response;
-      assert(stream instanceof ReadableStream);
-      expect(readableStreamToArray(stream, { catchError: true })).resolves.toMatchSnapshot();
-    } else {
-      expect(response).rejects.toThrow("test error");
-    }
-  },
-);
+  return {
+    url,
+    aigne,
+    close: () => server.stop(true),
+  };
+}
 
-test("AIGNEClient should return error response for not found agent", async () => {
-  const client = new AIGNEClient({ url });
+async function createAIGNE() {
+  const model = new OpenAIChatModel();
 
-  const response = client.call("not-exists-agent", {});
+  const chat = AIAgent.from({
+    name: "chat",
+  });
 
-  expect(response).rejects.toThrow("status 404: Agent not-exists-agent not found");
-});
-
-test("AIGNEClient should return error response for invalid request body", async () => {
-  const client = new AIGNEClient({ url });
-
-  const response = client.call("chat", [] as unknown as Message);
-
-  expect(response).rejects.toThrow(
-    "status 400: Call agent chat check arguments error: input: Expected object, received array",
-  );
-});
+  return new ExecutionEngine({ model, agents: [chat] });
+}

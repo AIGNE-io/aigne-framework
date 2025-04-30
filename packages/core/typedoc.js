8872ae3aed1e493f951c11a601445d8691276ae9
Yechao Li <liyechao@yechao.xyz>
2025-04-30 10:09:49 +0800

    chore: build typedoc in workspace root

--- packages/core/typedoc.js
+++ packages/core/typedoc.js
@@ -2,12 +2,6 @@
  * @type {import('typedoc').TypeDocOptions}
  */
 const config = {
-  plugin: [
-    "typedoc-plugin-no-inherit",
-    "typedoc-plugin-markdown",
-    "@aigne/typedoc-plugin-example-utils-ts",
-    "./typedoc/typedoc-sidebar-plugin.ts",
-  ],
   entryPoints: [
     "src/aigne/index.ts",
     "src/agents/*agent.ts",
@@ -16,33 +10,9 @@ const config = {
     "src/client/index.ts",
   ],
   entryPointStrategy: "expand",
-  sortEntryPoints: false,
-  sort: ["source-order", "static-first"],
   jsDocCompatibility: {
     exampleTag: false,
   },
-  readme: "README.md",
-  mergeReadme: true,
-  router: "module",
-  cleanOutputDir: true,
-  disableSources: true,
-  inheritNone: true,
-  formatWithPrettier: true,
-  useCodeBlocks: false,
-  expandObjects: true,
-  hidePageHeader: true,
-  indexFormat: "list",
-  parametersFormat: "table",
-  interfacePropertiesFormat: "table",
-  classPropertiesFormat: "table",
-  typeAliasPropertiesFormat: "table",
-  enumMembersFormat: "table",
-  propertyMembersFormat: "table",
-  typeDeclarationFormat: "table",
-  tableColumnSettings: {
-    hideModifiers: true,
-  },
-  removeExpressionsFromExamples: ["spyOn", "expect", "mock", "assert"],
 };
 
 export default config;

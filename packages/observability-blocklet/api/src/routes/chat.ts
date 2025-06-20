529ddc78e74c90095f69c315014b59d49584c545
leermao <447552708@qq.com>
2025-06-20 08:07:25 +0800

    chore: support observability blocklet

--- packages/observability-blocklet/api/src/routes/chat.ts
+++ packages/observability-blocklet/api/src/routes/chat.ts
@@ -1,22 +1,20 @@
-import { AIAgent, AIGNE } from "@aigne/core";
-import { OpenAIChatModel } from "@aigne/openai";
 import { Router } from "express";
 
 const router = Router();
 
-const aigne = new AIGNE({
-  model: new OpenAIChatModel({ apiKey: process.env.OPENAI_API_KEY, model: "gpt-4o-mini" }),
-});
+// const aigne = new AIGNE({
+//   model: new OpenAIChatModel({ apiKey: process.env.OPENAI_API_KEY, model: "gpt-4o-mini" }),
+// });
 
-const agent = AIAgent.from({
-  name: "chat",
-  instructions: "You are a friendly chatbot",
-});
+// const agent = AIAgent.from({
+//   name: "chat",
+//   instructions: "You are a friendly chatbot",
+// });
 
-router.get("/chat", async (_req, res) => {
-  const result = await aigne.invoke(agent, { message: "What is AIGNE?" });
-  console.log(result);
-  res.json(result);
-});
+// router.get("/chat", async (_req, res) => {
+//   const result = await aigne.invoke(agent, { message: "What is AIGNE?" });
+//   console.log(result);
+//   res.json(result);
+// });
 
 export default router;

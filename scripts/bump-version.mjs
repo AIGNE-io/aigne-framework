3da87fc83932d872bd3bc4dee32bb27d3a82f1b6
leermao <447552708@qq.com>
2025-06-24 09:21:01 +0800

    fix: examples test

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -2,27 +2,27 @@
 /* eslint-disable no-console */
 /* eslint-disable import/no-extraneous-dependencies */
 
-import { execSync } from 'child_process';
-import path from 'path';
+import { execSync } from "node:child_process";
+import path from "node:path";
 
-import { $, chalk, fs } from 'zx';
+import { fs, $, chalk } from "zx";
 
 execSync(
-  'bumpp --no-tag --no-commit --no-push package.json blocklets/*/package.json packages/*/package.json framework/*/package.json',
+  "bumpp --no-tag --no-commit --no-push package.json blocklets/*/package.json packages/*/package.json framework/*/package.json",
   {
-    stdio: 'inherit',
-  }
+    stdio: "inherit",
+  },
 );
 
-const { version } = await fs.readJSON('package.json');
-await fs.writeFileSync('version', version);
+const { version } = await fs.readJSON("package.json");
+await fs.writeFileSync("version", version);
 
 console.log(chalk.greenBright(`[info]: start to modify blocklets version to ${version}`));
-const dirPath = path.join(__dirname, '../blocklets');
+const dirPath = path.join(__dirname, "../blocklets");
 let pathList = await fs.readdirSync(dirPath);
 pathList = pathList.map((item) => `${dirPath}/${item}`);
 
-let newChangelog = '';
+let newChangelog = "";
 const now = new Date();
 const currentDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
 const title = `## ${version} (${currentDate})`;
@@ -31,7 +31,7 @@ try {
   const gitRes = await $`git log --pretty=format:"- %s" "main"...HEAD`;
   newChangelog = gitRes.stdout.trim();
 } catch {
-  console.error(chalk.redBright('Could not get git log, please write changelog manually.'));
+  console.error(chalk.redBright("Could not get git log, please write changelog manually."));
 }
 
 for (const ymlDir of pathList) {
@@ -39,17 +39,19 @@ for (const ymlDir of pathList) {
 
   try {
     await fs.ensureFile(`${ymlDir}/CHANGELOG.md`);
-    const oldChangelog = await fs.readFile(`${ymlDir}/CHANGELOG.md`, 'utf8');
-    const changelog = [title, newChangelog, oldChangelog].filter((item) => !!item).join('\n\n');
+    const oldChangelog = await fs.readFile(`${ymlDir}/CHANGELOG.md`, "utf8");
+    const changelog = [title, newChangelog, oldChangelog].filter((item) => !!item).join("\n\n");
     await fs.writeFile(`${ymlDir}/CHANGELOG.md`, changelog);
 
-    console.log(`\nNow you can make adjustments to ${chalk.cyan('CHANGELOG.md')}. Then press enter to continue.`);
+    console.log(
+      `\nNow you can make adjustments to ${chalk.cyan("CHANGELOG.md")}. Then press enter to continue.`,
+    );
   } catch (error) {
     console.error(error);
   }
 }
-console.log(chalk.greenBright('[info]: all blocklets version modified.'));
+console.log(chalk.greenBright("[info]: all blocklets version modified."));
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
-process.stdin.on('data', process.exit.bind(process, 0));
+process.stdin.on("data", process.exit.bind(process, 0));

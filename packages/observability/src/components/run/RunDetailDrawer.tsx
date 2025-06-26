c27703d5567fb8f69431d56b79c4d8656f6a872e
leermao <447552708@qq.com>
2025-06-26 17:46:02 +0800

    chore: the price spent on the token

--- packages/observability/src/components/run/RunDetailDrawer.tsx
+++ packages/observability/src/components/run/RunDetailDrawer.tsx
@@ -11,6 +11,7 @@ import RunStatsHeader from "./RunStatsHeader.tsx";
 import TraceDetailPanel from "./TraceDetailPanel.tsx";
 import TraceItemList from "./TraceItem.tsx";
 import type { TraceData } from "./types.ts";
+
 interface RunDetailDrawerProps {
   traceId: string | null;
   open: boolean;

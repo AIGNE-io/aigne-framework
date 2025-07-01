e08a9298d9caeac3eb56b62b1b081321fb1413f1
leermao <447552708@qq.com>
2025-07-01 12:14:40 +0800

    fix: copy script

--- packages/observability-api/scripts/build-ui.mjs
+++ packages/observability-api/scripts/build-ui.mjs
@@ -1,5 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import fs from "fs-extra";
 import { $ } from "zx";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -9,11 +10,13 @@ const apiPath = path.resolve(root, "observability-api");
 
 console.log("building ui...");
 console.log(`run "cd ${uiPath} && pnpm install && pnpm run build"`);
-await $`cd ${uiPath} && pnpm install && pnpm run build`;
+await $({ cwd: uiPath })`pnpm install`;
+await $({ cwd: uiPath })`pnpm run build`;
+
 console.log("removing dist...");
-console.log(`run "rm -rf ${apiPath}/dist"`);
-await $`rm -rf ${apiPath}/dist`;
+await fs.remove(path.join(apiPath, "dist"));
+
 console.log("copying dist...");
-console.log("run `cp -r ${uiPath}/dist ${apiPath}/dist`");
-await $`cp -r ${uiPath}/dist ${apiPath}/dist`;
+await fs.copy(path.join(uiPath, "dist"), path.join(apiPath, "dist"));
+
 console.log("done");

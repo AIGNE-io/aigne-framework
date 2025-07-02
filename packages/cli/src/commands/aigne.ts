dda84eecd9dfe894ac1adc937309064a59228d70
Yechao Li <liyechao@yechao.xyz>
2025-07-02 11:47:00 +0800

    feat(cli): support executing aigne.yaml via shebang (#!/usr/bin/env aigne)

--- packages/cli/src/commands/aigne.ts
+++ packages/cli/src/commands/aigne.ts
@@ -7,17 +7,21 @@ import { createRunCommand } from "./run.js";
 import { createServeMCPCommand } from "./serve-mcp.js";
 import { createTestCommand } from "./test.js";
 
-export function createAIGNECommand(): Command {
+export function createAIGNECommand({
+  aigneFilePath,
+}: {
+  aigneFilePath?: string;
+}): Command {
   console.log(asciiLogo);
 
   return new Command()
     .name("aigne")
     .description("CLI for AIGNE framework")
     .version(AIGNE_CLI_VERSION)
-    .addCommand(createRunCommand())
-    .addCommand(createTestCommand())
+    .addCommand(createRunCommand({ aigneFilePath }))
+    .addCommand(createTestCommand({ aigneFilePath }))
     .addCommand(createCreateCommand())
-    .addCommand(createServeMCPCommand())
+    .addCommand(createServeMCPCommand({ aigneFilePath }))
     .addCommand(createObservabilityCommand())
     .showHelpAfterError(true)
     .showSuggestionAfterError(true);

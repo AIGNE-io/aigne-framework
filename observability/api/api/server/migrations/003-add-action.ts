6d7835d111afbf15288dbe185220d216de426f12
leermao <447552708@qq.com>
2025-07-07 20:29:48 +0800

    chore: delete trace spans

--- observability/api/api/server/migrations/003-add-action.ts
+++ observability/api/api/server/migrations/003-add-action.ts
@@ -1,4 +1,7 @@
 export default {
   hash: "202507071830-add-action",
-  sql: [`ALTER TABLE Trace ADD COLUMN action INTEGER;`],
+  sql: [
+    `ALTER TABLE Trace ADD COLUMN action INTEGER;`,
+    "CREATE INDEX IF NOT EXISTS idx_trace_action ON Trace(action) WHERE action IS NOT NULL;",
+  ],
 };

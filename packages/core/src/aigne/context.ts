fb1ea7af409c0509a78f477f4eaf0211513f13c7
leermao <447552708@qq.com>
2025-07-08 09:03:54 +0800

    chore: code review

--- packages/core/src/aigne/context.ts
+++ packages/core/src/aigne/context.ts
@@ -515,12 +515,6 @@ export class AIGNEContext implements Context {
           const { error } = args[0] as ContextEventMap["agentFailed"][0];
           span.setStatus({ code: SpanStatusCode.ERROR, message: error.message });
 
-          await this.observer?.traceExporter
-            ?.upsertInitialSpan?.(span as unknown as ReadableSpan)
-            .catch((err) => {
-              logger.error("upsertInitialSpan error", err?.message || err);
-            });
-
           span.end();
 
           break;

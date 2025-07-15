0d68fffb1fc3e1e70ad4701899d5260dbc576d0c
leermao <447552708@qq.com>
2025-07-15 12:02:25 +0800

    chore: option

--- models/aigne-hub/test/index.ts
+++ models/aigne-hub/test/index.ts
@@ -1,13 +1,13 @@
-import { AIGNEHubChatModel } from "../src/aigne-hub-model.js";
+// import { AIGNEHubChatModel } from "../src/aigne-hub-model.js";
 
-const model = new AIGNEHubChatModel({
-  url: "https://bbqa32a2r3dqk3zk2klm7ufufo6ehiutaxemmapgtsm.did.abtnet.io/ai-kit/api/v2/chat",
-  accessKeyId: "blocklet-zC8uZzjvL9Ht3VUuzA6aN1M9dB2MRHEtdTkb4aPcivpBf",
-  model: "openai/gpt-4o-mini",
-});
+// const model = new AIGNEHubChatModel({
+//   url: "https://bbqa32a2r3dqk3zk2klm7ufufo6ehiutaxemmapgtsm.did.abtnet.io/ai-kit/api/v2/chat",
+//   accessKeyId: "blocklet-zC8uZzjvL9Ht3VUuzA6aN1M9dB2MRHEtdTkb4aPcivpBf",
+//   model: "openai/gpt-4o-mini",
+// });
 
-model
-  .invoke({ messages: [{ role: "user", content: "Hello, world!" }] }, { streaming: true })
-  .then((res) => {
-    console.log(res);
-  });
+// model
+//   .invoke({ messages: [{ role: "user", content: "Hello, world!" }] }, { streaming: true })
+//   .then((res) => {
+//     console.log(res);
+//   });

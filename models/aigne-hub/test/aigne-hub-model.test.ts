1be32e707ea31d645b67131347836bbaafdabb6a
leermao <447552708@qq.com>
2025-08-16 11:43:58 +0800

    chore: add test

--- models/aigne-hub/test/aigne-hub-model.test.ts
+++ models/aigne-hub/test/aigne-hub-model.test.ts
@@ -1,54 +1,9 @@
 import { describe, expect, spyOn, test } from "bun:test";
 import assert from "node:assert";
-import { AIAgent, AIGNE, ChatModel, isAgentResponseDelta } from "@aigne/core";
+import { ChatModel, isAgentResponseDelta } from "@aigne/core";
 import { stringToAgentResponseStream } from "@aigne/core/utils/stream-utils.js";
-import { OpenAIChatModel } from "@aigne/openai";
-import { AIGNEHTTPServer } from "@aigne/transport/http-server/index.js";
-import { serve } from "bun";
-import { detect } from "detect-port";
-import { Hono } from "hono";
 import { AIGNEHubChatModel } from "../src/index.js";
-
-async function createHonoServer() {
-  const port = await detect();
-  const url = `http://localhost:${port}/`;
-
-  const honoApp = new Hono();
-
-  const aigne = await createAIGNE();
-  const aigneServer = new AIGNEHTTPServer(aigne);
-
-  honoApp.post("/ai-kit/api/v2/chat", async (c) => {
-    return aigneServer.invoke(c.req.raw);
-  });
-
-  honoApp.get("/__blocklet__.js", async (c) => {
-    return c.json({
-      componentMountPoints: [
-        { did: "z8ia3xzq2tMq8CRHfaXj1BTYJyYnEcHbqP8cJ", mountPoint: "/ai-kit" },
-      ],
-    });
-  });
-
-  const server = serve({ port, fetch: honoApp.fetch });
-
-  return {
-    url,
-    aigne,
-    close: () => server.stop(true),
-  };
-}
-
-async function createAIGNE() {
-  const model = new OpenAIChatModel();
-
-  const chat = AIAgent.from({
-    name: "chat",
-    inputKey: "message",
-  });
-
-  return new AIGNE({ model, agents: [chat] });
-}
+import { createHonoServer } from "./_mocks_/server.js";
 
 describe("aigne-hub-model", () => {
   test("AIGNEHubChatModel example simple", async () => {

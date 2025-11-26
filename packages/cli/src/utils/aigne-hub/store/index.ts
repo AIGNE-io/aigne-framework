5027bb7ca7a0cda5f43e42411102a5258d40407c
leermao <447552708@qq.com>
2025-11-26 20:33:47 +0800

    fix: serviceName name

--- packages/cli/src/utils/aigne-hub/store/index.ts
+++ packages/cli/src/utils/aigne-hub/store/index.ts
@@ -37,7 +37,7 @@ const getSecretStore = async () => {
   if (!cachedSecretStore) {
     cachedSecretStore = await createSecretStore({
       filepath: AIGNE_ENV_FILE,
-      serviceName: "aigne-hub-keyring",
+      serviceName: "aigne-hub",
     });
   }
 
